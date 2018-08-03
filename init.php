72bada9fac0963fcc52c47216ad6aa82ea95e9ee
Sven Rymenants <sven@byteworks.be>
2018-08-03 19:11:51 +0200

    Use current directory for composer autoloader

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php
 
-if (is_file('vendor/autoload.php'))
+if (is_file(__DIR__.'/vendor/autoload.php'))
 {
-    require_once 'vendor/autoload.php';
+    require_once __DIR__.'/vendor/autoload.php';
 }
